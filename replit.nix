d2fbd7ce581eca580a09c0d4e73bb4e9aef03c53
Maga222006 <magapatahov@gmail.com>
2024-12-31 20:00:44 +0100

    Update Replit configuration with complete dependencies

--- replit.nix
+++ replit.nix
@@ -4,5 +4,14 @@
         pkgs.python39Packages.pip
         pkgs.portaudio
         pkgs.ffmpeg
+        pkgs.nodejs
+        pkgs.nodePackages.typescript
+        pkgs.libuuid
+        pkgs.openssl
+        pkgs.git
     ];
+    env = {
+        PYTHONBIN = "${pkgs.python39}/bin/python3.9";
+        LANG = "en_US.UTF-8";
+    };
 } 
