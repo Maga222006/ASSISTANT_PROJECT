759838ee7364e5f46ef6c5d52df2e3715df31535
Maga222006 <magapatahov@gmail.com>
2024-12-31 20:09:29 +0100

    Simplify Replit configuration for better compatibility

--- replit.nix
+++ replit.nix
@@ -1,17 +1,8 @@
 { pkgs }: {
     deps = [
-        pkgs.python39
-        pkgs.python39Packages.pip
+        pkgs.python310
+        pkgs.python310Packages.pip
         pkgs.portaudio
         pkgs.ffmpeg
-        pkgs.nodejs
-        pkgs.nodePackages.typescript
-        pkgs.libuuid
-        pkgs.openssl
-        pkgs.git
     ];
-    env = {
-        PYTHONBIN = "${pkgs.python39}/bin/python3.9";
-        LANG = "en_US.UTF-8";
-    };
 } 
