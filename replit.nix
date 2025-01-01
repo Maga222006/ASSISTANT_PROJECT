95ef530d373b2c0bb1e7398452ce0747857d42d2
Maga222006 <magapatahov@gmail.com>
2025-01-01 14:19:39 +0100

    Update replit.nix with additional build dependencies

--- replit.nix
+++ replit.nix
@@ -5,7 +5,22 @@
         pkgs.portaudio
         pkgs.ffmpeg
         pkgs.gcc
+        pkgs.gcc-unwrapped
+        pkgs.gcc-unwrapped.lib
+        pkgs.cmake
+        pkgs.gnumake
+        pkgs.pkg-config
         pkgs.python310Packages.wheel
         pkgs.python310Packages.setuptools
+        pkgs.alsa-lib
+        pkgs.libpulseaudio
+        pkgs.swig
     ];
+    env = {
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            pkgs.gcc-unwrapped.lib
+            pkgs.alsa-lib
+            pkgs.libpulseaudio
+        ];
+    };
 } 
